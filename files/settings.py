7cc829a898a1c6fdd5a4df6887e59099612280d6
Adilson Pavan <adilson.pavan@gmail.com>
2019-10-05 01:40:29 -0300

    remove files

--- files/settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-connection_string_atlas = "mongodb+srv://user_dev:arpatlas*2678@clusterdatascience-da3ej.mongodb.net/test?retryWrites=true&w=majority"
-
-# key and token from twitter api
-consumer_key = "ZqI0I1aBVy7hLBekaJI3YoRI9"
-consumer_secret = "hwXpx1vjNIt0Q2zvoelHVygDcYt1l8KDEA8GPLTTg61dPqCiAX"
-access_token = "1169801468922204160-ZxzBNso5fURif90KMuh01PeyYOkIvn"
-access_token_secret = "AUTJBtpy8dpfCc3fsN7G667SJfzVhgVDdVujv6zLoFMWJ"
